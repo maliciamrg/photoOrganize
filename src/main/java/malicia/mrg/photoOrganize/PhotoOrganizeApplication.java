028e8aeefa4a76044a0a9e7d84ae529ace3871f4
cleme <malcia.mrg@gmail.com>
2023-09-28 01:23:35 +0200

    init

---
+++ src/main/java/malicia/mrg/photoOrganize/PhotoOrganizeApplication.java
@@ -0,0 +1,13 @@
+package malicia.mrg.photoOrganize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoOrganizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoOrganizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/malicia/mrg/photoOrganize/PhotoOrganizeApplicationTests.java
@@ -0,0 +1,13 @@
+package malicia.mrg.photoOrganize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoOrganizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
